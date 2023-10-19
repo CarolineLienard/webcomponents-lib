14a4a6d588e002b4be518a7c73534d923d01625e
Caroline Lienard <>
2023-10-19 20:01:17 +0200

    🚧 work in progress

---
+++ src/components/form/inputs/standard-input.ts
@@ -0,0 +1,48 @@
+import { LitElement, html, css } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import "../../../styles/index.css";
+
+@customElement("standard-input")
+export class StandardInput extends LitElement {
+  @property({ type: String }) label = "Label*";
+  @property({ type: String }) placeholder = "Placeholder";
+
+  protected render() {
+    return html`
+      <div class="input-container">
+        <input id="input" placeholder="${this.placeholder}" />
+        <label for="input">${this.label}</label>
+      </div>
+    `;
+  }
+
+  static styles = css`
+    .input-container {
+      position: relative;
+    }
+
+    input {
+      background-color: var(--colorNeutralBackgroundStrong);
+      padding: 21px 8px 6px 8px;
+      border: 1px solid var(--colorEditionBorderLight);
+      border-radius: 8px;
+      font-size: 16px;
+      font-weight: 400;
+      color: var(--colorNeutralContentStrong);
+    }
+
+    input::placeholder {
+      color: var(--colorNeutralContentMedium);
+    }
+
+    label {
+      position: absolute;
+      top: 8px;
+      left: 8px;
+      color: #666;
+      font-size: 14px;
+      font-weight: 500;
+      color: var(--colorEditionContentDefault);
+    }
+  `;
+}
